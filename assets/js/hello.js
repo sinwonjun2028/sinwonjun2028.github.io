a2137f79c0ca5b4e249b98c6fde8cb0c6fff8dcd
sinwonjun2028 <sinwonjun2028@gmail.com>
2026-05-03 16:41:47 +0000

    페이지에 자바스크립트 삽입

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,8 +1,6 @@
-const author = "이상";
-const title = "날개";
-const year = 1936;
-const today = 2026;
-const yearsPassed = today- year;
-console.log(`안녕하세요!${author}의 「${title}」 (${year}) ` + `페이지에 오신 것을 환영합니다.`);
-console.log(`이 작품은 발표된 지${yearsPassed}년이 되었습니다.`);
+const author = "신원준";
+const title = "소비자학 전공";
+const year = 2026;
+console.log(`안녕하세요! ${author}(${title})의 ` + `페이지에 오신 것을 환영합니다.`);
+console.log(`이 사이트는 ${year}년에 개설되었습니다.`);
 
