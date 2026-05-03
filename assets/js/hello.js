965552d514ab5002ba8c4d096079ff8a3f3f1c3f
sinwonjun2028 <sinwonjun2028@gmail.com>
2026-05-03 16:49:06 +0000

    페이지에 자바스크립트 추가

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,6 +1,6 @@
 const author = "신원준";
 const title = "소비자학 전공";
 const year = 2026;
-console.log(`안녕하세요! ${author}(${title})의 ` + `페이지에 오신 것을 환영합니다.`);
-console.log(`이 사이트는 ${year}년에 개설되었습니다.`);
+console.log('안녕하세요! ${author}(${title})의 페이지에 오신 것을 환영합니다.');
+console.log('이 사이트는 ${year}년에 개설되었습니다.');
 
